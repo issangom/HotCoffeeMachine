63256df027e799e762f4af926c564b493a53f00d
issangom <61689058+issangom@users.noreply.github.com>
2022-09-06 19:28:14 +0200

    test unitaire a terminer

---
+++ HotCoffeeMachineTest/DrinkOrderTest.cs
@@ -0,0 +1,21 @@
+namespace HotCoffeeMachineTest
+{
+    public class DrinkOrderTest
+    {
+
+        //[TestCase(3, 1, "M:2.00")]
+        //[TestCase(0.20, 0, "M:0.20")]
+        //[TestCase(4.5, 3, "M:1.50")]
+        public void Process_Should_Return__Amount_OFCoffee(double orderPrice, string expectedMessage)
+        {
+            // GIVEN
+            
+
+            // WHEN
+           
+
+            // THEN
+            
+        }
+    }
+}
